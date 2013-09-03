4ad1495e1dbf1c5b706e689b760e6fdfd19668be
odin-asen <geroge.forman@freenet.de>
2013-09-03 19:57:23 +0200

    ActionBar hat Knöpfe zum Sortieren der Liste.

--- src/com/github/trainingsapp/WorkoutListActivity.java
+++ src/com/github/trainingsapp/WorkoutListActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Toast;
 import com.github.R;
 import com.github.trainingsapp.business.Exercise;
 import com.github.trainingsapp.views.DetailPagerFragment;
@@ -59,6 +63,32 @@ public class WorkoutListActivity extends FragmentActivity implements AdapterView
 
     /* ActionBar Titel aendern */
     getActionBar().setTitle(item.getName());
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.mainmenu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_sort_muscle:
+        Toast.makeText(this, "Nach Muskeln sortieren", Toast.LENGTH_SHORT)
+            .show();
+        break;
+      case R.id.action_sort_name:
+        Toast.makeText(this, "Nach Name sortieren", Toast.LENGTH_SHORT)
+            .show();
+        break;
+
+      default:
+        break;
+    }
+
+    return true;
   }
 
   /*   End   */
